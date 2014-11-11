9465ca7bdf28bfc3dde8bd40a8589f3b768a6314
Budai Laszlo <Laszlo.Budai@balabit.com>
2014-11-11 10:44:09 +0100

    smake: using BalabitBuilder
    Signed-off-by: Budai Laszlo <Laszlo.Budai@balabit.com>

---
+++ smake/configure_opts.py
@@ -0,0 +1,8 @@
+default_opts = ("--disable-agent --disable-applications "
+                "--disable-manuals --disable-embedded-perl "
+                "--with-default-snmp-version=3 --with-openssl "
+                "--with-sys-contact=root --with-sys-location=Unknown "
+                "--with-logfile=none "
+                "--with-persistent-directory=/opt/syslog-ng/var/snmp "
+                "--without-perl-modules --enable-reentrant "
+                "--with-openssl=\${prefix}")
